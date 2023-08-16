9188aae459696acae1c67392b3ed65b66428340d
Dipendra Misra <dimisra@dipendras-mbp.myfiosgateway.com>
2023-08-16 02:03:32 -0400

    More changes. Almost ready

--- verbal_gym/envs/summarization/pick_best_summary.py
+++ verbal_gym/envs/summarization/pick_best_summary.py
@@ -1,4 +1,5 @@
 import pdb
+import random
 
 from verbal_gym.gpt.gpt import GPT3
 
@@ -8,7 +9,10 @@ class PickBestSummary:
         Select the best summary
     """
 
-    def __init__(self, num_actions=4, summary_max_tokens=50, feedback_max_tokens=40):
+    Binary, LOGPROB = range(2)
+
+    def __init__(self, num_actions=4, summary_max_tokens=50, fixed=True, seed=1234,
+                 feedback_max_tokens=40, reward_type="binary"):
 
         from datasets import load_dataset
 
@@ -22,6 +26,17 @@ class PickBestSummary:
 
         self.summary_max_tokens = summary_max_tokens
         self.feedback_max_tokens = feedback_max_tokens
+        self.fixed = fixed
+
+        self.seed = seed
+        self.rng = random.Random(seed)
+
+        if reward_type == "binary":
+            self.reward_type = PickBestSummary.Binary
+        elif reward_type == "logprob":
+            self.reward_type = PickBestSummary.LOGPROB
+        else:
+            raise AssertionError(f"Unhandled reward_type {reward_type}. Can only handle: binary, logprob")
 
         # Prompts for summary generation
         self.good_summary_prompt = "You are given the article below. \n Article: %s. \n Write a good, " \
@@ -37,12 +52,14 @@ class PickBestSummary:
 
         self.ctr = 0
         self.current_article = None
-        self.current_candidate_summaries = None
+        self.current_summaries_with_reward = None
 
     def _generate_summaries(self):
 
         if self.current_article is None:
             raise AssertionError("You need to do reset first.")
+
+        num_bad_summaries = int(self.num_actions / 2.0)
 
         # Suppose we have K actions, i.e., summaries to generate
         # Generate 1 good summary using argmax and temperature 0
@@ -51,56 +68,95 @@ class PickBestSummary:
 
         summaries_with_score = []
 
-        # Good summary
+        # Good summaries
         good_prompt = self.good_summary_prompt % self.current_article
-        summary, info = self.summary_generator_llm.generate(prompt=good_prompt,
-                                                            max_tokens=self.summary_max_tokens,
-                                                            logprobs=1,
-                                                            temperature=0.0)
+        for i in range(0, self.num_actions - num_bad_summaries):
 
-        summaries_with_score.append((summary, info["logprob"]))
+            temperature = 0.0 if i == 0 else 1.0
+            generation, info = self.summary_generator_llm.generate(prompt=good_prompt,
+                                                                   max_tokens=self.summary_max_tokens,
+                                                                   echo=True,
+                                                                   logprobs=
+                                                                   1 if self.reward_type == self.LOGPROB else None,
+                                                                   temperature=temperature)
 
-        # int(K/2) bad summaries
+            if good_prompt != generation[:len(good_prompt)]:
+                raise AssertionError(f"{generation} should start with {good_prompt}")
+
+            summary = generation[len(good_prompt):]
+
+            if self.reward_type == self.LOGPROB:
+                reward = info["logprob"]
+            elif self.reward_type == self.Binary:
+                reward = 1.0 if i == 0 else 0.0
+            else:
+                raise AssertionError(f"Unhandled reward type {self.reward_type}")
+
+            summaries_with_score.append((summary, reward))
+
+        # Bad summaries
         bad_prompt = self.bad_summary_prompt % self.current_article
+        for _ in range(0, num_bad_summaries):
 
-        bad_summaries = int(self.num_actions / 2.0)
-        for _ in range(0, bad_summaries):
+            generation, info = self.summary_generator_llm.generate(prompt=bad_prompt,
+                                                                   max_tokens=self.summary_max_tokens,
+                                                                   logprobs=None,
+                                                                   temperature=1.0)
 
-            summary, info = self.summary_generator_llm.generate(prompt=bad_prompt,
-                                                                max_tokens=self.summary_max_tokens,
-                                                                logprobs=1,
-                                                                temperature=1.0)
-            summaries_with_score.append((summary, info["logprob"]))
+            if generation != generation[:len(bad_prompt)]:
+                raise AssertionError(f"{generation} should start with {bad_prompt}")
 
-        for _ in range(0, self.num_actions - 1 - bad_summaries):
+            summary = generation[len(bad_prompt):]
 
-            summary, info = self.summary_generator_llm.generate(prompt=bad_prompt,
-                                                                max_tokens=self.summary_max_tokens,
-                                                                logprobs=1,
-                                                                temperature=1.0)
-            summaries_with_score.append((summary, info["logprob"]))
+            if self.reward_type == self.LOGPROB:
+                # Compute logprob of the summary under the new summary
+                good_prompt_with_bad_summary = good_prompt + summary
+                reward = self.summary_generator_llm.get_logprobs(prompt=good_prompt_with_bad_summary)
+            elif self.reward_type == self.Binary:
+                reward = 0
+            else:
+                raise AssertionError(f"Unhandled reward type {self.reward_type}")
+
+            summaries_with_score.append((summary, reward))
+
+        random.shuffle(summaries_with_score)
 
     def reset(self):
 
-        article = self.dataset["train"][self.ctr]["article"]
-        self.ctr = (self.ctr + 1) % len(self.dataset["train"])
+        if self.current_article is None:
+            # First run so set the starting counter position
+            self.ctr = self.rng.randint(0, len(self.dataset["train"]) - 1)
 
-        candidate_summaries = self._generate_summaries()
-        observation = {
-            "article": article,
-            "actions": candidate_summaries
-        }
-        info = {
-            "help": "You are given an article in text and a list of summaries. Pick a summary indexed as "
-                    "0 to num_summaries - 1 that is the most approprriate summary."
-        }
+        if not self.fixed or self.current_article is None:
+            self.current_article = self.dataset["train"][self.ctr]["article"]
+            self.current_summaries_with_reward = self._generate_summaries()
+            self.ctr = (self.ctr + 1) % len(self.dataset["train"])
 
-        return observation, info
+        observation = f"You are given an article below. \n {self.current_article}. " \
+                      f"You are also given {self.num_actions} many summaries some of which are good " \
+                      f"and others can be incorrect. The list of summaries and their indices are:"
+
+        for idx, summary_with_reward in enumerate(self.current_summaries_with_reward):
+            summary, _ = summary_with_reward
+            observation = observation + f"\n {idx}: {summary}"
+
+        observation = observation + "\n Please select the best summary for the above article by entering its index."
+
+        return observation
 
     def step(self, action):
 
         feedback = self.critic_llm.generate(
-            prompt=self.feedback_prompt % (self.current_article, self.current_candidate_summaries[action]),
+            prompt=self.feedback_prompt % (self.current_article, self.current_summaries_with_reward[action][0]),
             max_tokens=self.feedback_max_tokens
         )
-        return feedback
+
+        # The next observation doesn't make sense but instead of returning None, I return the chosen action description
+        next_observation = self.current_summaries_with_reward[action][0]
+        reward = self.current_summaries_with_reward[action][1]
+        done = True
+        info = {
+            "feedback": feedback
+        }
+
+        return next_observation, reward, done, info
