736ae9f32beeb0d9c97f1f7ab07000489e8486fb
chinganc <chinganc0@gmail.com>
2023-08-22 20:09:37 -0700

    Make BasicAgent work with freefrom problems.

--- verbal_gym/agents/utils.py
+++ verbal_gym/agents/utils.py
@@ -2,6 +2,9 @@ import random
 from verbal_gym.utils.misc_utils import extract_int
 
 def extract_action(response, n_actions, separator="#"):
+    if n_actions is None:  # free form action
+        action = response.split(separator)[1]
+        return action
     try:
         action = response.split(separator)[1]
         if action.isnumeric():
