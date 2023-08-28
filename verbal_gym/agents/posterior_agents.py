a48e296cbd8f841182a1b168fba534a9394b9cdd
Dipendra Misra <dimisra@dipendras-mbp.myfiosgateway.com>
2023-08-28 13:16:01 -0400

    PEP compliance

--- verbal_gym/agents/posterior_agents.py
+++ verbal_gym/agents/posterior_agents.py
@@ -41,18 +41,25 @@ class PosteriorAgent(BasicAgent):
 
     def update_history(self, feedback):
         # XXX Add random permuation and paraphrasing
+
         if self.paraphrase_at_given:  # Here we just paraphrase the feedback when it is given.
-            history_text ='None'
-            if len(self.history)>0:
+            history_text = 'None'
+
+            if len(self.history) > 0:
                 self.history[-1]['feedback'] = self.paraphrase(feedback)
                 history = np.random.permutation(self.history) if self.permute_history else self.history
-                history_text = '\n'.join([ indent(f'{self.action_name}: {item["action"]}\n\nFeedback: {item["feedback"]}\n\n\n','\t') for item in history])
+                history_text = '\n'.join(
+                    [indent(f'{self.action_name}: {item["action"]}\n\nFeedback: {item["feedback"]}\n\n\n','\t')
+                     for item in history])
+
         else:  # Here we do paraphrasing when we need to return history observations.
-            history_text='None'
-            if len(self.history)>0:
+            history_text = 'None'
+            if len(self.history) > 0:
                 self.history[-1]['feedback'] = feedback
                 history = np.random.permutation(self.history) if self.permute_history else self.history
-                history_text = '\n'.join([ indent(f'{self.action_name}: {item["action"]}\n\nFeedback: {self.paraphrase(item["feedback"])}\n\n\n','\t') for item in history])
+                history_text = '\n'.join(
+                    [indent(f'{self.action_name}: {item["action"]}\n\nFeedback: {self.paraphrase(item["feedback"])}\n\n\n','\t')
+                     for item in history])
         return history_text
 
     def paraphrase(self, sentence):
