28c0346bc9f6a7d54098032fad3ad28344f5ec3e
Dipendra Misra <dimisra@dipendras-mbp.myfiosgateway.com>
2023-08-27 20:46:25 -0400

    More changes

---
+++ scripts/model_based_agent.py
@@ -0,0 +1,70 @@
+import gym
+
+from llm.gpt.gpt import GPT3
+from verbal_gym.llm.gpt_models import GPT
+from verbal_gym.llm.openai_utils import init_openai_api
+from verbal_gym.agents.agents import RandomAgent, BasicAgent
+from verbal_gym.agents.posterior_agents import PosteriorAgent, ParaphraseAgent
+from verbal_gym.utils.utils import evaluate_agent, set_seed
+from verbal_gym.utils.misc_utils import print_color
+
+
+def main(args):
+
+    n_episodes = args.n_episodes
+    horizon = args.horizon
+
+    # Create the environment
+    env = gym.make(args.env_name)
+    set_seed(args.seed, env)
+
+    critic_llm = GPT3()
+
+    assert isinstance(env.action_space, gym.spaces.Discrete), "Currently only handles discrete actions"
+
+    dataset = []
+    for _ in range(args.model_eps):
+
+        obs = env.reset()
+
+        for action in range(env.action_space.n):
+
+            simulated_feedback = critic_llm.generate(
+                prompt=f"I am trying to solve a problem described as {env.docstring}."
+                       f"I was presented with a specific problem {obs}. "
+                       f"I chose action {action}. How did I do?",
+                max_tokens=100,
+                temperature=0.0)
+
+            dataset.append((obs, action, simulated_feedback))
+
+    # Create a prompt using the above dataset
+    pass
+
+    # Create prompt to take action based on the above feedback
+    scores = evaluate_agent(gpt_agent, env, horizon=horizon, n_episodes=n_episodes, n_workers=args.n_workers)
+    print_color('Basic Posterior agent: mean score {:.2f}, std {:.2f}'.format(scores.mean(), scores.std()), 'red')
+    return scores
+
+
+def get_parser():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_episodes', type=int, default=10)
+    parser.add_argument('--horizon', type=int, default=10)
+    parser.add_argument('--env_name',type=str, default='verbal-BanditTenArmedRandomRandom-v0')
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--n_workers', type=int, default=1)
+    parser.add_argument('--temperature', type=float, default=0.0)
+    parser.add_argument('--not_paraphrase', action='store_true')
+    parser.add_argument('--not_permute_history', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--model', type=str, default='azure:gpt-35-turbo')
+    parser.add_argument('--model_eps', type=str, default='azure:gpt-35-turbo')
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    main(parser.parse_args())
---
+++ verbal_gym/agents/model_based_agents.py
@@ -0,0 +1,53 @@
+from verbal_gym.agents.agents import BasicAgent
+from verbal_gym.agents.utils import extract_action
+from verbal_gym.utils.misc_utils import print_color
+
+import numpy as np
+from textwrap import dedent, indent
+
+
+class ParaphraseAgent:
+    system_prompt = dedent("""
+    You are an expert in paraphrasing. You will see a sentence and you need to
+    paraphrase it compactly, while keeping the numerics.
+    """)
+
+    def __init__(self, llm):
+        self.llm = llm
+        self.promot_template = dedent("""\
+            You see the following sentence:
+            {}
+            Paraphrase:
+        """)
+
+    def paraphrase(self, sentence):
+        response, _ = self.llm.generate(self.promot_template.format(sentence))
+        return response
+
+
+class ModelBasedAgent(BasicAgent):
+
+    def __init__(self, llm, n_actions, verbose=False, action_name='Action',
+                permute_history=True, paraphrase_agent=None):
+        super().__init__(llm, n_actions, verbose=verbose, action_name=action_name)
+        self.permute_history = permute_history
+        self.paraphrase_agent = paraphrase_agent
+
+    def reset(self, docstring):
+        self._docstring = docstring
+
+    def update_history(self, feedback):
+        world_info='None'
+        if len(self.history)>0:
+            self.history[-1]['feedback'] = feedback
+            # XXX Add random permuation and paraphrasing
+            history = np.random.permutation(self.history) if self.permute_history else self.history
+            world_info = '\n'.join([ indent(f'{self.action_name}: {item["action"]}\n\nFeedback: {self.paraphrase(item["feedback"])}\n\n\n','\t') for item in history])
+        return world_info
+
+    def paraphrase(self, sentence):
+        return self.paraphrase_agent.paraphrase(sentence) if self.paraphrase_agent is not None else sentence
+
+    @property
+    def docstring(self):
+        return self.paraphrase(self._docstring)
