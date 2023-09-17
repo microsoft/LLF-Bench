8505c7988045e5689123910e2c94aac9b1b218dc
Dipendra Misra <dimisra@dipendras-mbp.myfiosgateway.com>
2023-09-16 20:24:26 -0400

    Separate oracle agent from basic agent.

--- scripts/basic_agent.py
+++ scripts/basic_agent.py
@@ -1,9 +1,10 @@
 import gym
-from verbal_gym.llm.gpt_models import GPT
+import argparse
+
 from verbal_gym.llm import make_llm
 from verbal_gym.agents.basic_agent import BasicAgent
-from verbal_gym.agents.random_agent import RandomAgent
-from verbal_gym.utils.utils import evaluate_agent, set_seed
+from verbal_gym.utils.utils import set_seed
+from verbal_gym.utils.utils import evaluate_agent
 from verbal_gym.utils.misc_utils import print_color
 
 
@@ -31,44 +32,19 @@ def main(args):
     scores = evaluate_agent(gpt_agent, env, horizon=horizon, n_episodes=n_episodes, n_workers=args.n_workers)
     print_color('Basic LLM agent: mean score {:.2f}, std {:.2f}'.format(scores.mean(), scores.std()), 'red')
 
-    if n_actions is not None:  # finite action, so we can test RandomAgent and FullInformationAgent as well.
-        # Random agent
-        random_agent = RandomAgent(n_actions)
-        scores = evaluate_agent(random_agent, env, horizon=horizon, n_episodes=n_episodes, n_workers=args.n_workers)
-        print_color('Random agent: mean score {:.2f}, std {:.2f}'.format(scores.mean(), scores.std()), 'red')
-
-        from verbal_gym.envs.env_wrapper import FullInformationWrapper
-        from verbal_gym.agents.basic_agent import FullInformationAgent
-        env = FullInformationWrapper(env)
-
-        # Full information agent
-        system_prompt = FullInformationAgent.system_prompt
-        llm = GPT(system_prompt)
-        gpt_agent = FullInformationAgent(llm,
-                                         n_actions=n_actions,
-                                         verbose=args.verbose)
-
-        scores = evaluate_agent(gpt_agent,
-                                env,
-                                horizon=horizon,
-                                n_episodes=n_episodes,
-                                n_workers=args.n_workers,
-                                return_full_information=True)
-        print_color('Oracle LLM agent: mean score {:.2f}, std {:.2f}'.format(scores.mean(), scores.std()), 'red')
-
 
 def get_parser():
 
-    import argparse
-
     parser = argparse.ArgumentParser()
+
     parser.add_argument('--n_episodes', type=int, default=10)
     parser.add_argument('--horizon', type=int, default=10)
-    parser.add_argument('--env_name',type=str, default='verbal-BanditTenArmedRandomRandom-v0')  #'verbal-Haiku-v0'
+    parser.add_argument('--env_name',type=str, default='verbal-BanditTenArmedRandomRandom-v0')
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--n_workers', type=int, default=1)
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--model', type=str, default='azure:gpt-35-turbo')
+
     return parser
 
 
