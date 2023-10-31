a8120196e7f76193612c4a88be17d44d8d7eb8a2
Dipendra Misra <dimisra@Dipendras-MacBook-Pro.local>
2023-10-31 16:44:47 -0400

    Renamed gridworld, updated docstring calculation, partial draft for feedback_type

--- verbal_gym/envs/gridworld/gridworld.py
+++ verbal_gym/envs/gridworld/gridworld.py
@@ -8,18 +8,32 @@ from verbal_gym.envs.gridworld.room import Room
 from verbal_gym.envs.gridworld.scene import Scene
 
 
-class RandomizedGridworld(gym.Env):
+class Gridworld(gym.Env):
 
-    # Feedback level
-    Bandit, Gold, Oracle = range(3)
+    # Basic (b), partial (p), and complete (c)
+    INSTRUCTION_TYPES = ('b', 'p', 'c')
 
-    def __init__(self, num_rooms=20, horizon=20, fixed=True, feedback_level="gold", min_goal_dist=4):
-        super(RandomizedGridworld, self).__init__()
+    # Feedback type:
+    # n: none
+    # m: mixed
+    # r: reward
+    # hn: hindsight negative
+    # hp: hindsight positive
+    # fn: future negative
+    # fp: future positive
+    FEEDBACK_TYPES = ('n', 'm', 'r', 'hn', 'hp', 'fn', 'fp')
+
+    # feedback_level="gold"
+    def __init__(self, num_rooms=20, horizon=20, fixed=True, instruction_type="c", feedback_type="hp", min_goal_dist=4):
+        super(Gridworld, self).__init__()
 
         # Action space consists of 4 actions: North, South, East and West
         self.num_actions = 4
         self.action_space = gym.spaces.Discrete(self.num_actions)
         self.observation_space = gym.spaces.Text(sys.maxsize)
+
+        self.instruction_type = instruction_type
+        self.feedback_type = feedback_type
 
         self.num_rooms = num_rooms
         self.horizon = horizon
@@ -28,27 +42,17 @@ class RandomizedGridworld(gym.Env):
 
         self.fixed = fixed
 
-        self.docstring = "You are in a house with multiple rooms. Each room can have objects that will be visible to " \
-                         "you if you are in that room. Each room can have a door along the North, South, East and " \
-                         "West direction. You can follow a direction to go from one room to another. " \
-                         "If there is no door along that direction, then you will remain in the room. You will start " \
-                         "in a room. Your goal is to navigate to another room which has the treasure. You have an action" \
-                         "space of size 4. Action 0 leads to going North. Action 1 leads to going East. " \
-                         "Action 2 leads going west. Action 3 leads to going South."
-
-        if feedback_level == "bandit":
-            self.feedback_level = RandomizedGridworld.Bandit
-
-        elif feedback_level == "gold":
-            self.feedback_level = RandomizedGridworld.Gold
-
-        elif feedback_level == "oracle":
-            self.feedback_level = RandomizedGridworld.Oracle
-
-        else:
-            raise AssertionError(f"Unhandled feedback level {feedback_level}")
+        # Docstring
+        self.base_docstring = "You are in a house with multiple rooms. Each room can have objects that will " \
+                              "be visible to you if you are in that room. Each room can have a door along the " \
+                              "North, South, East and West direction. You can follow a direction to go from one room " \
+                              "to another. If there is no door along that direction, then you will remain in the " \
+                              "room. You will start in a room. Your goal is to navigate to another room which has " \
+                              "the treasure. You have an action space of size 4. Action 0 leads to going North. " \
+                              "Action 1 leads to going East. Action 2 leads going west. Action 3 leads to going South."
 
         # Counters that may have to be reset
+        self.docstring = None
         self.current_timestep = 0.0
         self.current_scene = None
         self.current_room = None
@@ -143,8 +147,55 @@ class RandomizedGridworld(gym.Env):
         self.goal_prev_visited = False
 
         obs = self.make_room_obs(self.current_room)
+        self.docstring = self.generate_docstring()
 
-        return obs
+        return dict(instruction=self.docstring,
+                    observation=obs,
+                    feedback=None)
+
+    def generate_docstring(self):
+
+        if self.instruction_type == "b":
+            # Basic docstring
+            docstring = self.base_docstring
+
+        elif self.instruction_type == "p":
+            # Partial docstring.
+            docstring = self.base_docstring + " " + self.get_optimal_path_desc(partial=True)
+
+        elif self.instruction_type == "c":
+            # Complete docstring. Optimal policy can be achieved using just the docstring
+            docstring = self.base_docstring + " " + self.get_optimal_path_desc(partial=False)
+        else:
+            raise AssertionError(f"Unhandled feedback_level {self.instruction_type}")
+
+        return docstring
+
+    def get_optimal_path_desc(self, partial=False):
+
+        optimal_path = self.current_scene.get_optimal_path()
+
+        if len(optimal_path) == 0:
+            path_descps = ["Congratulations! You are already in the room with treasure."]
+        else:
+            path_descps = ["I will also provide you a path towards reaching your goal."]
+
+        for ix, (direction, room) in enumerate(optimal_path):
+            if ix == 0:
+                path_descps.append(f"First, you follow {direction} direction, to reach the room {room.get_name()}.")
+            elif ix == len(optimal_path) - 1:
+                path_descps.append(f"Next, you follow {direction} direction, to reach the room {room.get_name()}.")
+            else:
+                path_descps.append(f"Finally, you follow {direction} direction, to reach the "
+                                   f"room {room.get_name()} which has treasure.")
+
+        if partial:
+            r = 0.4 + random.random() * 0.2
+            partial_len = int(len(path_descps) * r)
+            opt_path_desc = " ".join(path_descps[:partial_len])
+        else:
+            opt_path_desc = " ".join(path_descps)
+        return opt_path_desc
 
     def log_env(self, logger):
         self.current_scene.log_scene(logger)
@@ -176,15 +227,46 @@ class RandomizedGridworld(gym.Env):
         self.current_timestep += 1
         done = self.current_timestep == self.horizon
 
-        if self.feedback_level == RandomizedGridworld.Bandit:
-            if reward == 1.0:
-                feedback = f"You succeeded! Congratulations."
-            elif self.goal_prev_visited:
-                feedback = f"You have already reached the treasure. Good job."
-            else:
-                feedback = f"You didn't succeed. Trying visiting more rooms."
+        feedback = self.generate_feedback(self, old_gold_action, new_room)
 
-        elif self.feedback_level == RandomizedGridworld.Gold:
+        info = {
+            "feedback": feedback
+        }
+
+        next_packed_obs = dict(instruction=self.docstring,
+                               observation=next_obs,
+                               feedback=feedback)
+
+        return next_packed_obs, reward, done, info
+
+    def generate_feedback(self, old_gold_action, new_room, feedback_type=None):
+
+        if feedback_type is None:
+            feedback_type = self.feedback_type
+
+        if feedback_type == "n":        # None
+            feedback = "No feedback is available."
+
+        elif feedback_type == "r":      # Reward described in text
+            feedback = f"You got a reward of {reward}."
+            # if reward == 1.0:
+            #     feedback = f"You got a reward of 1.0"
+            # elif self.goal_prev_visited:
+            #     feedback = f"You have already reached the treasure. Good job."
+            # else:
+            #     feedback = f"You didn't succeed. Trying visiting more rooms."
+
+        elif feedback_type == "m":      # Mixed feedback type
+
+            sampled_feedback_type = random.choice(["n", "r", "hp", "hn", "fn", "fp"])
+            feedback = self.generate_feedback(old_gold_action=old_gold_action,
+                                              new_room=new_room,
+                                              feedback_type=sampled_feedback_type)
+
+        elif feedback_type == "hn":     # Hindsight negative
+            raise NotImplementedError()
+
+        elif feedback_type == "hp":     # Hindsight positive
 
             if reward == 1.0:
                 feedback = f"You succeeded! Congratulations."
@@ -217,7 +299,10 @@ class RandomizedGridworld(gym.Env):
                     # Case 3: If the agent takes the right transition
                     feedback = f"Good job. You took the right direction and moved closer to the treasure."
 
-        elif self.feedback_level == RandomizedGridworld.Oracle:
+        elif feedback_type == "fn":     # Future negative
+            pass
+
+        elif feedback_type == "fp":     # Future positive
 
             if reward == 1.0:
                 feedback = f"You succeeded! Congratulations."
@@ -256,17 +341,13 @@ class RandomizedGridworld(gym.Env):
                                f"room you should move towards the {gold_action} direction."
 
         else:
-            raise AssertionError(f"Unhandled feedback level {self.feedback_level}")
+            raise AssertionError(f"Unhandled feedback level {feedback_type}")
 
-        info = {
-            "feedback": feedback
-        }
-
-        return next_obs, reward, done, info
+        return feedback
 
 
 if __name__ == "__main__":
-    env = RandomizedGridworld(num_rooms=10, feedback_level="oracle")
+    env = Gridworld(num_rooms=10)
     obs = env.reset()
     print(obs)
     # pdb.set_trace()
