30d50a82a4732feffdb1f013dd0a51df01b5480f
chinganc <chinganc0@gmail.com>
2023-11-06 14:44:32 -0800

    Rename gym_bandits to bandits

--- verbal_gym/envs/__init__.py
+++ verbal_gym/envs/__init__.py
@@ -1,5 +1,5 @@
 from verbal_gym.envs import gridworld
-from verbal_gym.envs import gym_bandits
+from verbal_gym.envs import bandits
 from verbal_gym.envs import loss_landscape
 from verbal_gym.envs import metaworld
 from verbal_gym.envs import movie_rec
