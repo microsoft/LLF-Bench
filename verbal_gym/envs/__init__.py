33cbff9f72124d54bd267fa48f73a60463d98e8a
adith387 <adith387@gmail.com>
2023-11-06 16:01:08 -0800

    Initial commit of highway wrapper

--- verbal_gym/envs/__init__.py
+++ verbal_gym/envs/__init__.py
@@ -5,4 +5,4 @@ from verbal_gym.envs import metaworld
 from verbal_gym.envs import movie_rec
 from verbal_gym.envs import poem_env
 from verbal_gym.envs import highway
-from verbal_gym.envs import alfworld
+#from verbal_gym.envs import alfworld   # Commenting until we add the install instructions for AlfWorld
