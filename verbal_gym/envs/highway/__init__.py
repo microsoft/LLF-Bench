d2e8a4587b4135db69cbe3ae0b94b437b74508e8
adith387 <adith387@gmail.com>
2023-11-09 19:46:19 -0800

    Highway parking env passes test_env checks

--- verbal_gym/envs/highway/__init__.py
+++ verbal_gym/envs/highway/__init__.py
@@ -10,30 +10,16 @@ ENVIRONMENTS = (
 
 def make_env(env_name,
              instruction_type='b',
-             feedback_type='r',
+             feedback_type='a',
              ):
     """ Make the original env and wrap it with the VerbalGymWrapper. """
     env = gym.make(env_name)
     return HighwayWrapper(env, instruction_type=instruction_type, feedback_type=feedback_type)
 
-
-configs = generate_combinations_dict(
-                dict(env_name=ENVIRONMENTS,
-                     feedback_type=HighwayWrapper.FEEDBACK_TYPES,
-                     instruction_type=HighwayWrapper.INSTRUCTION_TYPES))
-
-for config in configs:
-    env_name, version = config['env_name'].split('-v')
-    register(
-        id=f"verbal-{env_name}-{config['instruction_type']}-{config['feedback_type']}-v{version}",
-        entry_point='verbal_gym.envs.highway:make_env',
-        kwargs=config,
-    )
-
 for env_name in ENVIRONMENTS:
     # default version (backwards compatibility)
     register(
-        id=f"verbal-{env_name}",
+        id=f"verbal-highway-{env_name}",
         entry_point='verbal_gym.envs.highway:make_env',
-        kwargs=dict(env_name=env_name, feedback_type='r', instruction_type='b')
+        kwargs=dict(env_name=env_name, feedback_type='a', instruction_type='b')
     )
--- verbal_gym/envs/highway/prompts.py
+++ verbal_gym/envs/highway/prompts.py
@@ -6,15 +6,25 @@ highway_instruction = (
     "You will receive the observation of the vehicle's state, the world state, as well as the desired parking location represented by a json string. "+
     "Your action is a 2-dim vector, where the first dimension controls the throttle input, and the last dimension controls the steering input. "+
      "Throttle is a number between -5 and 5, representing acceleration in units of m/s^2. "+
-     "Steering is a number between -pi/4 and pi/4, representing the steering angle in radians."
+     "Steering is a number between -pi/4 and pi/4, representing the steering angle in radians.",
 )
 
 b_instruction = (
     "Output a good action in the form of [throttle input, steering input].",
+    "Render a proper action in the style of [throttle input, steering input].",
+    "Supply a good action in the form of [throttle input, steering input].",
+    "Indicate a good action using [throttle input, steering input] as the format.",
+    "Come up with a good action in the form of [throttle input, steering input].",
+    "Present a correct action in the form of [throttle input, steering input].",
 )
 
 r_feedback = (
     "You received a reward of {reward}.",
+    "Your reward is {reward}.",
+    "The reward you get is {reward}.",
+    "You get a reward of {reward}.",
+    "The reward is {reward}.",
+    "You have obtained a reward of {reward}.",
 )
 
 hp_feedback = (
@@ -23,4 +33,7 @@ hp_feedback = (
 
 hn_feedback = (
     "This is a bad action, because the vehicle crashed.",
+    "This action is unfavorable, given that the vehicle crashed.",
+    "This action is problematic, because it caused the vehicle to crash.",
+    "This is an inappropriate action, as it resulted in the vehicle crashing.",
 )
