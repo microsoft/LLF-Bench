76c5270e86a38d8ba54234f31701f2a3ee256afa
Dipendra Misra <dimisra@Dipendras-MacBook-Pro.local>
2023-11-29 12:29:05 -0500

    Added test for gridworld and alfworld

---
+++ tests/test_gridworld.py
@@ -0,0 +1,10 @@
+import verbal_gym
+
+env = verbal_gym.make('verbal-Gridworld-v0')
+
+obs = env.reset()
+for _ in range(5):
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+    for k, v in obs.items():
+        print(f"Took action {action} and received observation with key={k}, value={v}")
