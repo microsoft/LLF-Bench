0cdbb32675058f93769e8c697b5d00c1aa7e0635
Dipendra Misra <dimisra@Dipendras-MacBook-Pro.local>
2023-11-30 13:05:58 -0500

    Minor

--- llfbench/envs/alfworld/__init__.py
+++ llfbench/envs/alfworld/__init__.py
@@ -3,14 +3,14 @@ from gymnasium.envs.registration import register
 
 from llfbench.envs.alfworld.alfworld import Alfworld
 from llfbench.envs.alfworld.wrapper import AlfworldWrapper
-from llfbench.envs.alfworld.alfworld_download import main
+from llfbench.envs.alfworld.alfworld_download import download_alfworld_data
 
 # TODO download PPDL and other data necessary to run Alfworld
 os.environ["ALFWORLD_DATA"] = "alfworld_data"
 
-if not os.path.exists(os.environ["ALFWORLD_DATA"]):
+if not os.path.exists(os.environ["ALFWORLD_DATA"]) or len(os.environ["ALFWORLD_DATA"]) == 0:
     print(f"Downloading Alfworld data to {os.environ['ALFWORLD_DATA']}")
-    main()
+    download_alfworld_data()
 else:
     print(f"Alfworld data already exists in {os.environ['ALFWORLD_DATA']} "
           f"If this is old or stale, then delete it and run the code again.")
